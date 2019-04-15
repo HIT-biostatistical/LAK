ebc9540865e77176478fbee428cdb2b9d445c2e3
HIT-biostatistical <48672780+HIT-biostatistical@users.noreply.github.com>
2019-04-15 09:12:19 +0800

    Add files via upload

--- R Scripts/ARI_test_for_diff_tissue.R
+++ R Scripts/ARI_test_for_diff_tissue.R
@@ -42,9 +42,19 @@ library(mclust)
 #save(ES, file = "RData")
 load("RData/human_data-5.RData")
 
-LAK_ann <- LAK(ES, num_cluster = 5 ,normed = T)
+#LAK_ann <- LAK(ES, num_cluster = 5 ,normed = T)
+#after_reduce_dim <- Rtsne(t(ES))
 
-ARIS <- LAK_ann
+load("RData/Figure_S5.RData")
+
+ES_plot_matrix <- data.frame(after_reduce_dim$Y,cluster = as.factor(LAK_ann[[1]]$Cs))
+ggplot(ES_plot_matrix,aes(x = X1,y = X2,shap = cluster,color = cluster)) +
+  geom_point() +
+  labs(x = "tSNE1",y = "tSNE2") + theme_classic() +
+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())
+
+
+ARIS <- c()
 for(k in 1:100){
   LAK_ann <- LAK(ES,num_cluster = 5 , normed = T , s_value = 18.2875)
   ARI <-adjustedRandIndex(LAK_ann[[1]]$Cs, cell_types)
@@ -53,3 +63,6 @@ for(k in 1:100){
 }
 
 load("RData/match__human_5_tissue_ARI_results.RData")
+
+
+
