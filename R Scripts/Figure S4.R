0656f386b9b7cb027bc9341784e4210fad9005a2
HIT-biostatistical <48672780+HIT-biostatistical@users.noreply.github.com>
2019-05-04 23:55:03 +0800

    Update Figure S4.R

--- R Scripts/Figure S4.R
+++ R Scripts/Figure S4.R
@@ -5,81 +5,85 @@ library( plyr )
 library(SingleCellExperiment)
 library(mclust)
 
-# st <-F 
-# for (i in 1:5){
-#   sc_new <- get_sc_data_new(i)
-#   if(st == T){
-#     ES_ite <- sc_new[[1]]
-#     if(ncol(ES_ite) > 100){
-#       ES_ite <- ES_ite[,sample(ncol(ES_ite),100)]
-#     }
-#     ES_ite <- as.data.frame(ES_ite)
-#     ES_ite$gene <- rownames(ES_ite)
-#     cat("ES_ite", i,'\n')
-#     ES <- join(ES, ES_ite, type = "inner", by = "gene")
-#     cat("join", "\n")
-#     cell_types <- c(cell_types,rep(i,ncol(ES_ite)-1))
-#   }else{
-#     
-#     ES <- sc_new[[1]]
-#     
-#     if(ncol(ES) > 100)  
-#     {
-#       ES <- ES[,sample(ncol(ES),100)]
-#     }
-#     
-#     ES <- as.data.frame(ES)
-#     cell_types <- rep(i,ncol(ES))
-#     ES$gene <- rownames(ES)
-#     cat("ES", i)
-#     st <- T
-#   }
-# }
-# 
-# 
-# rm(ES_ite,sc_new)
-# ES <- subset(ES, select= -gene)
-#save(ES, file = "RData")
-load("RData/human_data-5.RData")
+st <-F
+for (i in 1:5){
+  sc_new <- get_sc_data_new(i)
+  if(st == T){
+    ES_ite <- sc_new[[1]]
+    if(ncol(ES_ite) > 100){
+      ES_ite <- ES_ite[,sample(ncol(ES_ite),100)]
+    }
+    ES_ite <- as.data.frame(ES_ite)
+    ES_ite$gene <- rownames(ES_ite)
+    cat("ES_ite", i,'\n')
+    ES <- join(ES, ES_ite, type = "inner", by = "gene")
+    cat("join", "\n")
+    cell_types <- c(cell_types,rep(i,ncol(ES_ite)-1))
+  }else{
+
+    ES <- sc_new[[1]]
+
+    if(ncol(ES) > 100)
+    {
+      ES <- ES[,sample(ncol(ES),100)]
+    }
+
+    ES <- as.data.frame(ES)
+    cell_types <- rep(i,ncol(ES))
+    ES$gene <- rownames(ES)
+    cat("ES", i)
+    st <- T
+  }
+}
+
+
+rm(ES_ite,sc_new)
+ES <- subset(ES, select= -gene)
+#load("RData/human_data-5-new.RData")
 
 LAK_ann <- LAK(ES, num_cluster = 5 ,normed = T)
 
-ARIS <- c()
+ARIS <- adjustedRandIndex(LAK_ann[[1]]$Cs, cell_types)
+
+reco <- c()
+records <- list()
+n <- 1
 for(k in 1:100){
-  LAK_ann <- LAK(ES,num_cluster = 5 , normed = T , s_value = 18.2875)
+  LAK_ann <- LAK(ES,num_cluster = 5 , normed = T , s_value =  19.03125)
   ARI <-adjustedRandIndex(LAK_ann[[1]]$Cs, cell_types)
   cat(ARI,'\n')
   ARIS <- c(ARIS, ARI)
+  if(!(ARI %in% reco)){
+    reco <- c(reco, ARI)
+    records[[n]] <- LAK_ann
+    n <- n+1
+  }
 }
-load("RData/match_human_5_tissue_ARI_results.RData")
 
-#Output the LAK_ann and after_reduce_dim result under ARI=0.940, 0.827, 0.706 
-#LAK_ann <- LAK(ES, num_cluster = 5 ,normed = T)
-#ari <-adjustedRandIndex(LAK_ann[[1]]$Cs, cell_types)
-#after_reduce_dim <- Rtsne(t(ES))
-load("RData/Figure_S4.RData")
+table(ARIS)
 
-#plot visualization of cluster result under ARI = 0.940
-S4a_ES_plot_matrix <- data.frame(S4a_after_reduce_dim$Y,cluster = as.factor(S4a_LAK_ann[[1]]$Cs))
-Fig.S4a <- ggplot(S4a_ES_plot_matrix,aes(x = X1,y = X2,shap = cluster,color = cluster)) +
+LAK_ann <- records[[1]]
+adjustedRandIndex(LAK_ann[[1]]$Cs, cell_types)
+tsne_df <- Rtsne(t(ES),perplexity = 15, dims = 2)
+LAK_ann_f <- as.factor(LAK_ann[[1]]$Cs)
+plot_matrix <- data.frame(tsne_df$Y,cluster = LAK_ann_f)
+ggplot(plot_matrix,aes(x = X1,y = X2,shap = cluster,color = cluster)) +
   geom_point() +
   labs(x = "tSNE1",y = "tSNE2") + theme_classic() +
-  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())
-Fig.S4a
+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank()) 
 
-#plot visualization of cluster result under ARI = 0.827
-S4b_ES_plot_matrix <- data.frame(S4b_after_reduce_dim$Y,cluster = as.factor(S4b_LAK_ann[[1]]$Cs))
-Fig.S4b <- ggplot(S4b_ES_plot_matrix,aes(x = X1,y = X2,shap = cluster,color = cluster)) +
+LAK_ann <- records[[2]]
+adjustedRandIndex(LAK_ann[[1]]$Cs, cell_types)
+tsne_df <- Rtsne(t(ES),perplexity = 15, dims = 2)
+LAK_ann_f <- as.factor(LAK_ann[[1]]$Cs)
+plot_matrix <- data.frame(tsne_df$Y,cluster = LAK_ann_f)
+ggplot(plot_matrix,aes(x = X1,y = X2,shap = cluster,color = cluster)) +
   geom_point() +
   labs(x = "tSNE1",y = "tSNE2") + theme_classic() +
-  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())
-Fig.S4b
+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank()) 
 
-#plot visualization of cluster result under ARI = 0.706
-S4c_ES_plot_matrix <- data.frame(S4c_after_reduce_dim$Y,cluster = as.factor(S4c_LAK_ann[[1]]$Cs))
-Fig.S4c <- ggplot(S4c_ES_plot_matrix,aes(x = X1,y = X2,shap = cluster,color = cluster)) +
-  geom_point() +
-  labs(x = "tSNE1",y = "tSNE2") + theme_classic() +
-  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())
-Fig.S4c
 
+
+#save(ARIS,file = "RData/match__human_5_tissue_ARI_results.RData")
+load("RData/match__human_5_tissue_ARI_results.RData")
+table(ARIS)
