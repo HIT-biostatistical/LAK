5ff038e4c97c18e58e8338da5de569f70bae6774
HIT-biostatistical <48672780+HIT-biostatistical@users.noreply.github.com>
2019-05-23 16:55:30 +0800

    Update Figure S4.R

--- R Scripts/Figure S4.R
+++ R Scripts/Figure S4.R
@@ -1,4 +1,4 @@
-setwd("~/LAK")
+setwd("E:/LAK")
 source("LAK.R")
 source("R Scripts/get_data.R")
 library( plyr )
@@ -20,14 +20,14 @@ for (i in 1:5){
     cat("join", "\n")
     cell_types <- c(cell_types,rep(i,ncol(ES_ite)-1))
   }else{
-
+    
     ES <- sc_new[[1]]
-
+    
     if(ncol(ES) > 100)
     {
       ES <- ES[,sample(ncol(ES),100)]
     }
-
+    
     ES <- as.data.frame(ES)
     cell_types <- rep(i,ncol(ES))
     ES$gene <- rownames(ES)
@@ -39,7 +39,7 @@ for (i in 1:5){
 
 rm(ES_ite,sc_new)
 ES <- subset(ES, select= -gene)
-#load("RData/human_data-5-new.RData")
+load("RData/human_data-5-new.RData")
 
 LAK_ann <- LAK(ES, num_cluster = 5 ,normed = T)
 
@@ -62,28 +62,50 @@ for(k in 1:100){
 
 table(ARIS)
 
+tsne_df <- Rtsne(t(ES),perplexity = 15, dims = 2)
+
 LAK_ann <- records[[1]]
 adjustedRandIndex(LAK_ann[[1]]$Cs, cell_types)
-tsne_df <- Rtsne(t(ES),perplexity = 15, dims = 2)
-LAK_ann_f <- as.factor(LAK_ann[[1]]$Cs)
-plot_matrix <- data.frame(tsne_df$Y,cluster = LAK_ann_f)
+LAK_ann_f_1 <- as.factor(LAK_ann[[1]]$Cs)
+plot_matrix <- data.frame(tsne_df$Y,cluster = LAK_ann_f_1)
 ggplot(plot_matrix,aes(x = X1,y = X2,shap = cluster,color = cluster)) +
   geom_point() +
   labs(x = "tSNE1",y = "tSNE2") + theme_classic() +
   theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank()) 
+
+
 
 LAK_ann <- records[[2]]
 adjustedRandIndex(LAK_ann[[1]]$Cs, cell_types)
-tsne_df <- Rtsne(t(ES),perplexity = 15, dims = 2)
-LAK_ann_f <- as.factor(LAK_ann[[1]]$Cs)
-plot_matrix <- data.frame(tsne_df$Y,cluster = LAK_ann_f)
+LAK_ann_f_2 <- LAK_ann[[1]]$Cs
+#exchang cluster id for same color annotation in ggplot
+for(n in 1:length(LAK_ann_f_2)){
+  if(LAK_ann_f_2[n] == 5){
+    LAK_ann_f_2[n] <- 8
+  }else if(LAK_ann_f_2[n] == 3){
+    LAK_ann_f_2[n] <- 10
+  }else if(LAK_ann_f_2[n]==4){
+    LAK_ann_f_2[n] <- 6
+  }
+}
+
+for(n in 1:length(LAK_ann_f_2)){
+  if(LAK_ann_f_2[n] == 10){
+    LAK_ann_f_2[n]  <- 5
+  }else if(LAK_ann_f_2[n] == 8){
+    LAK_ann_f_2[n]  <- 4
+  }else if(LAK_ann_f_2[n]==6){
+    LAK_ann_f_2[n]  <- 3
+  }
+}
+
+plot_matrix <- data.frame(tsne_df$Y,cluster = as.factor(LAK_ann_f_2))
 ggplot(plot_matrix,aes(x = X1,y = X2,shap = cluster,color = cluster)) +
   geom_point() +
   labs(x = "tSNE1",y = "tSNE2") + theme_classic() +
   theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank()) 
 
 
-
-#save(ARIS,file = "RData/match__human_5_tissue_ARI_results.RData")
-load("RData/match__human_5_tissue_ARI_results.RData")
+save(ARIS,file = "RData/match__human_5_tissue_ARI_results.RData")
+#load("RData/match__human_5_tissue_ARI_results.RData")
 table(ARIS)
